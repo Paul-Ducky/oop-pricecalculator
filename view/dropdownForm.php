205f1cbfc02d3f70a80badd48d00708dfec86ffe
Paul Sempels <paulsempels@gmail.com>
2021-03-23 11:16:37 +0100

    created form, header&footer

---
+++ view/dropdownForm.php
@@ -0,0 +1,28 @@
+<?php require 'includes/header.php';?>
+    <div class="container">
+        <h2>Select the product, and customer!</h2>
+        <form method="post" class="form-inline">
+            <div class="form-group">
+                <label for="sel1">Select product:</label>
+                <select name="product" class="form-control col-6" id="sel1">
+                    <option value="1">test</option>
+                    <option value="2">test</option>
+                    <?php foreach($products AS $product):?>
+                        <option value="<?php echo $product['productID']?>"><?php echo $product['productName'] ?></option>
+                    <?php endforeach;?>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="sel2">Select customer:</label>
+                <select name="customer" class="form-control col-6" id="sel2">
+                    <option value="1">test</option>
+                    <option value="2">test</option>
+                    <?php foreach($customers AS $customer):?>
+                        <option value="<?php echo $customer['customerID']?>"><?php echo $customer['customerName'] ?></option>
+                    <?php endforeach;?>
+                </select>
+            </div>
+            <button type="submit" name="calculate" class="btn btn-primary" id="submit">Submit</button>
+        </form>
+    </div>
+<?php require 'includes/footer.php';?>
