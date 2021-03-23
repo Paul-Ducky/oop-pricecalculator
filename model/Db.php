8fab78b8a020eb17278f7bc19e400008c39c0d4d
Paul Sempels <paulsempels@gmail.com>
2021-03-23 09:56:21 +0100

    Created base file structure

---
+++ model/Db.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Db
+{
+    private const HOST = "localhost";
+    private string $user = "";
+    private string $pwd = "";
+    private string $dbName = "oop_calc";
+
+    public function connect() : PDO
+    {
+        $dsn = 'mysql:host=' . self::HOST . ';dbname=' . $this->dbName;
+        $pdo = new PDO($dsn, $this->user, $this->pwd);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    }
+}
