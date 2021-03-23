ef4fdd1c2e47c9f6eac844a4fa0ee3d5217eea92
Peter <dhondt.peter@gmail.com>
2021-03-23 12:27:20 +0100

    created classes Product and ProductLoader

--- loader/ProductLoader.php
+++ loader/ProductLoader.php
@@ -1,7 +1,40 @@
 <?php
+declare(strict_types=1);
 
+require_once 'Product.php';
+require_once 'Db.php';
 
 class ProductLoader
 {
+    private array $productArray = [];
 
+    public function getProduct(int $productID): Product
+    {
+        $DB = new Db();
+        $conn = $DB->connect();
+
+        $stmt = $conn->prepare("SELECT id, name, price FROM product WHERE id = :productID");
+        $stmt->bindValue('productID', $productID);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        $product = new Product ((int)$result['id'], $result['name'], (int)$result['price']);
+
+        return $product;
+    }
+
+    public function getAllProducts(): array
+    {
+        $DB = new Db();
+        $conn = $DB->connect();
+
+        $stmt = $conn->prepare("SELECT id, name, price FROM product");
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+        foreach($results as $result) {
+            $product = new Product((int)$result['id'], $result['name'], (int)$result['price']);
+            array_push($this->productArray, $product);
+        }
+
+        return $this->productArray;
+    }
 }
--- model/Product.php
+++ model/Product.php
@@ -1,7 +1,47 @@
 <?php
-
+declare(strict_types=1);
 
 class Product
 {
+    private int $productID;
+    private string $productName;
+    private int $productPrice;
+
+    public function __construct($productID, $productName, $productPrice)
+    {
+        $this->productID = $productID;
+        $this->productName = $productName;
+        $this->productPrice = $productPrice;
+    }
+
+    public function getProductID(): int
+    {
+        return $this->productID;
+    }
+
+    public function setProductID(int $productID): void
+    {
+        $this->productID = $productID;
+    }
+
+    public function getProductName(): string
+    {
+        return $this->productName;
+    }
+
+    public function setProductName(string $productName): void
+    {
+        $this->productName = $productName;
+    }
+
+    public function getProductPrice(): int
+    {
+        return $this->productPrice;
+    }
+
+    public function setProductPrice(int $productPrice): void
+    {
+        $this->productPrice = $productPrice;
+    }
 
 }
