a82517a75c5cdf7a5b59878be72309ba23defe74
Paul Sempels <paulsempels@gmail.com>
2021-03-24 14:13:25 +0100

    prep for login

--- view/includes/header.php
+++ view/includes/header.php
@@ -22,10 +22,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link px-4" href="#">Placeholder</a>
+                        <a class="nav-link px-4" href="#">Register!</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link px-4" href="#">Placeholder</a>
+                        <a class="nav-link px-4" href="/view/loginForm.php">Log in!</a>
                     </li>
                 </ul>
             </div>
---
+++ view/loginForm.php
@@ -0,0 +1,23 @@
+<?php require 'includes/header.php'; ?>
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-6">
+            <h2>Login</h2>
+            <form action="login.php" method="post">
+                <div class="form-group">
+                    <label for="username">Username: </label>
+                    <input type="text" name="username" id="username" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="password">Password: </label>
+                    <input type="password" name="password" id="password" class="form-control" required>
+                </div>
+                <button type="submit" class="btn-primary"> Login!</button>
+            </form>
+        </div>
+    </div>
+
+</div>
+<?php require 'includes/footer.php';
